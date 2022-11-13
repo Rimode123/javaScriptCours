07bb2b76fe60b1ab9e990f82bef1a19a4235d501
Rimode123 <rim.zouggari.idrissi@gmail.com>
2022-11-13 20:09:41 +0100

    add js code to arrays

---
+++ tableaux.js
@@ -0,0 +1,21 @@
+
+// tableau ==> une variable qui ne permet de stocker plusieur valeur à la fois
+// tableau ==> les valeurs sont indexés (ils commencent par l'indice 0)
+
+var ville = ["valenciennes","lille","paris"];
+
+//console.log("les villes est "+ ville);
+
+var first =ville[0];
+
+console.log("la premiere ville est: "+first);
+
+/* var second =ville[1];
+console.log("la deuxieme ville est: "+second); */
+
+// Combien de villes dans le tableau ==> length
+
+var size = ville.length ;
+console.log("il y a " +size +" villes dans le tableau");
+
+
