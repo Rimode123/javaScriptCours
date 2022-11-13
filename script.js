600400d26809a9af5db48dc69346c1d6b6189a9a
Rimode123 <rim.zouggari.idrissi@gmail.com>
2022-11-13 16:07:34 +0100

    add declaration variable and condition

---
+++ script.js
@@ -0,0 +1,70 @@
+
+
+
+/*
+// Declaration du variable (entier , chaine de caractaire, boolean)
+
+var nom="Rim";
+var age=28;
+var celib=true;
+
+console.log("le nom :"+nom + ",l'age :"+age)
+*/
+
+/*
+________________________________________________
+calcule 
+________________________________________________
+
+var nomb1=10;
+var nomb2=5;
+
+var somme =nomb1 + nomb2 ;
+var Soust =nomb1 - nomb2 ;
+var div =nomb1 / nomb2 ;
+var multi =nomb1 * nomb2 ;
+
+
+console.log("la sommme est :"+somme)
+*/
+/*______________________________________
+
+// Condition imbriqué
+
+
+var pays="Maroc";
+
+
+if(pays =="Maroc"){
+
+    console.log("le pays est maroc");
+
+}else if(pays=="France"){
+
+    console.log("le pays est france");
+
+
+
+}else{
+    console.log("le pays est inconnu");
+}
+*/
+ 
+/*
+_____________________________________________
+Les condition avec Switch 
+_____________________________________________
+*/
+
+var jour ="teletrav"
+
+switch(jour){
+    case "off":
+    console.log("C'est Mercredi :)");
+    break;
+
+    case "off":
+    console.log("C'est Dimanche");
+    break;
+
+}
