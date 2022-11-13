e8f647c7f5fb7cc2491a8fc01c4f0befd1de7e16
Rimode123 <rim.zouggari.idrissi@gmail.com>
2022-11-13 19:13:51 +0100

    the functions

---
+++ fonctions.js
@@ -0,0 +1,10 @@
+// Fonction ==> permet de controler l'execution des instruction
+// Fonction ==> permet de ne pas repeter le code 
+
+function afficher(nom){
+
+    console.log("Hello "+nom);
+}
+
+afficher("CDA Team");
+
