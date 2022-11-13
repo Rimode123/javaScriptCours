3c89de3dcf422b34293c0d612a65a3eef6e45a6d
Rimode123 <rim.zouggari.idrissi@gmail.com>
2022-11-13 17:28:16 +0100

    How to use the For() loop

---
+++ boucleFor.js
@@ -0,0 +1,25 @@
+// Traitement à répéter 
+
+// for (start ; condition ; incrementation = i+1 )
+/*
+//Afficher la phrase 5 fois 
+
+var i ;
+
+for (i = 0 ; i < 5 ; i++){
+    //i++ = i+1
+
+    console.log("Hello in rim js code");
+}
+*/
+
+//Afficher les nombres de 1 à 5
+
+var i ;
+
+for (i = 1 ; i <= 5 ; i++){
+    //i++ = i+1
+
+    console.log(i);
+
+}
--- operateurLogique.js
+++ operateurLogique.js
@@ -1,2 +1,20 @@
 // && = and  ! = not  || = or
 
+
+// && = Et
+
+var pays = "Maroc";
+var age = 28 ;
+
+if(pays == "Maroc" && age >= 30 ){
+
+    console.log("Admis");
+}
+
+// || = ou
+
+/*if(pays == "Maroc" || age >= 30 ){
+
+    console.log("Admis");
+}*/
+
