de1aa1ca3fb7f060220075b9b1eae71eb0eca4c6
Rimode123 <rim.zouggari.idrissi@gmail.com>
2022-11-14 10:27:37 +0100

    add methods string and number

---
+++ stringNumberMethods.js
@@ -0,0 +1,29 @@
+// REMPLACER LES LETTRES 
+
+var nom="Rim";
+
+console.log(nom.replace("R","B"));
+
+//________________________________________
+
+//Number Methods Math.pow ,Math.abs ,Math.floor,toFixed
+
+var n=3;
+// 3*3
+
+console.log(Math.pow(3,2));
+
+// -3 ==> 3
+
+var n=-3;
+console.log(Math.abs(n));
+
+// 7.5 ===> 7 INGNORER LA VERGULE
+
+var n=7.5;
+console.log(Math.floor(n));
+
+// 7.56432 ==> 7.56
+
+var n=7.56432;
+console.log(n.toFixed(2));
